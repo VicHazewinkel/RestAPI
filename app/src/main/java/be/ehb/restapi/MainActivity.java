238aa33abab7e9eed2d8faba36d8e6b6b452119e
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2023-12-11 15:03:56 +0100

    API Request

--- app/src/main/java/be/ehb/restapi/MainActivity.java
+++ app/src/main/java/be/ehb/restapi/MainActivity.java
@@ -3,12 +3,43 @@ package be.ehb.restapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tvContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvContent = findViewById(R.id.tv_content);
+        Thread thisWillRunInTheBackground = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    OkHttpClient mClient = new OkHttpClient();
+
+                    Request mRequest = new Request.Builder()
+                            .url("https://jsonplaceholder.typicode.com/posts")
+                            .get()
+                            .build();
+
+
+                    Response mResponse = mClient.newCall(mRequest).execute();
+
+                    String responseText = mResponse.body().string();
+                    tvContent.setText(responseText);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thisWillRunInTheBackground.start();
     }
 }
