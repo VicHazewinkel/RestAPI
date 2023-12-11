26eafe04969ba5ce8d792b87c788df521e835896
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2023-12-11 15:50:07 +0100

    Commit

--- app/src/main/java/be/ehb/restapi/MainActivity.java
+++ app/src/main/java/be/ehb/restapi/MainActivity.java
@@ -1,10 +1,19 @@
 package be.ehb.restapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Placeholder;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import be.ehb.restapi.model.Entitys;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -32,12 +41,39 @@ public class MainActivity extends AppCompatActivity {
 
                     Response mResponse = mClient.newCall(mRequest).execute();
 
+                    //raw payload data
                     String responseText = mResponse.body().string();
-                    tvContent.setText(responseText);
+                    //convert to json
+                    JSONArray postsJSONArray = new JSONArray(responseText);
+                    int postJSONArrayLenth = postsJSONArray.length();
+                    ArrayList<Entitys> parseObjects = new ArrayList<>(postJSONArrayLenth);
 
-                } catch (Exception e) {
-                    e.printStackTrace();
+                    for(int i = 0; i < postJSONArrayLenth; i++){
+                        JSONObject temp = postsJSONArray.getJSONObject(i);
+
+                        Entitys currentPost = new Entitys(
+                                temp.getLong("userId"),
+                                temp.getLong("id"),
+                                temp.getString("title"),
+                                temp.getString("body")
+                        );
+
+                        parseObjects.add(currentPost);
+
+                    }
+
+                    tvContent.setText(parseObjects.toString());
+
+
+                                                //
+                } catch (IOException e) {       // "Exception" kan ook maar dan heb je geen idee wat er mis is
+                                                // en de tweede "catch (JSONException e)" is dan niet nodig
+                                                //
+                    e.printStackTrace();    //this will crash the app
+                } catch (JSONException e) {
+                    throw new RuntimeException(e); //this will crash the app
                 }
+
             }
         });
         thisWillRunInTheBackground.start();
